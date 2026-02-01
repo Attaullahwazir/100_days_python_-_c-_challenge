d11d0fb632a66ca3c5e01908541a13ae6d4e524d
Attaullahwazir <attaullah.wazir.cybersec@gmail.com>
2026-02-02 02:17:34 +0500

    Delete Day1 directory

--- Day1/Day1_python.py
+++
@@ -1,4 +0,0 @@
-print("welcome to 100 days of challenge")
-print("I trust my self that i complete this challenge ")
-print("now lets write sentence that every programmer write it their life")
-print("Hello, world!")
