d153e4de229f00a361f9605fa1e9753259c7ebfa
Attaullahwazir <attaullah.wazir.cybersec@gmail.com>
2026-02-04 02:28:07 +0500

    Add files via upload

---
+++ Day3/Day3_Program2_py.py
@@ -0,0 +1,8 @@
+num1 = int(input("Enter the Number 1: "))
+num2 = int(input("Enter the Number 2: "))
+print(f"These are the values Number 1 =  {num1} and Number 2 = {num2} before swapping...")
+
+temp = num1
+num1 = num2
+num2 = temp
+print(f"These are the values Number 1 = {num1} and Number 2 = {num2} after swapping...")
