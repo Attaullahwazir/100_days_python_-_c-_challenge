ee5a54a4b83630cde4a889a57597d5ffafe7c1f6
Attaullahwazir <attaullah.wazir.cybersec@gmail.com>
2026-02-05 04:58:22 +0500

    Add files via upload

---
+++ Day4/Day4_Program2_py.py
@@ -0,0 +1,7 @@
+width = float(input("Enter the width of the rectangle: "))
+height = float(input("Enter the height of the rectangle: "))
+
+area = width * height
+perimeter = 2 * (width + height)
+print(f"the width {width} and height {height} of area in rectangle is {area}")
+print(f"the width {width} and height {height} of perimeter in rectangle is {perimeter}")
