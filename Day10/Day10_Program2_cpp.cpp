5dabd3d6a27b75bcaa42fe90413a0aebf2228ddf
Attaullahwazir <attaullah.wazir.cybersec@gmail.com>
2026-02-11 03:08:58 +0500

    Add files via upload

---
+++ Day10/Day10_Program2_cpp.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int num;
+	cout<<"Enter the Number: "<<endl;
+	cin>>num;
+	
+	int count = 0;
+	while (num > 0){
+	    int last_digit = num % 10;
+	    count += 1;
+	    num /= 10;
+	}
+	
+	cout<<"Total digits in that number is: "<<count<<endl;
+	
+	return 0;
+}
