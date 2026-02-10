c0946b813699a587ed4152c944e4ff7a17c40d15
Attaullahwazir <attaullah.wazir.cybersec@gmail.com>
2026-02-11 03:17:01 +0500

    Delete Day10 directory

--- Day10/Day10_Program1_py.py
+++
@@ -1,8 +0,0 @@
-num = int(input("Enter a number: "))
-
-sum = 0
-while num > 0:
-    last_digit = num % 10  # get the last digit
-    sum += last_digit      # add the last digit to the sum
-    num //= 10             # remove the last digit
-print(f"The sum of the digits is: {sum}")
